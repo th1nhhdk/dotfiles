9cb8d1f03b900e58429dec5f57008f091fce3e18
th1nhhdk <therealthinhhdk@gmail.com>
2021-11-20 14:57:59 +0700

    Update polybar

--- polybar/scripts/dunst.sh
+++ polybar/scripts/dunst.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ $(dunstctl is-paused) == "false" ]]
+if [[ $(dunstctl is-paused) == "true" ]]
 then
   polybar-msg hook dunst-toggle 1
-  dunstctl set-paused true
+  dunstctl set-paused false
 else
   polybar-msg hook dunst-toggle 2
-  dunstctl set-paused false
+  dunstctl set-paused true
 fi
